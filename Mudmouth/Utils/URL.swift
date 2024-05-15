2acbb85b249f2f21c54778771eb8178122e2e76d
Xie Zhihao <xzh1206@gmail.com>
2024-05-15 23:47:33 +0800

    Refine IP-related methods

--- Mudmouth/Utils/URL.swift
+++ Mudmouth/Utils/URL.swift
@@ -1,21 +1,13 @@
 import Foundation
+import Network
 import SwiftASN1
 
 extension URL {
     var ipv4: ASN1OctetString? {
         let host = self.host!
-        let sections = host.split(separator: ".")
-        var octets: [UInt8] = []
-        for section in sections {
-            let octet = UInt8(section)
-            if octet == nil {
-                return nil
-            }
-            octets.append(octet!)
-        }
-        if octets.count != 4 {
+        guard let ip = IPv4Address(host) else {
             return nil
         }
-        return ASN1OctetString(contentBytes: octets.prefix(upTo: 4))
+        return ASN1OctetString(contentBytes: ArraySlice<UInt8>(ip.rawValue))
     }
 }
